d3ac6259722bcc105aab3635171cc4e5842c6777
levan mchedlishvili <golocogo4@gmail.com>
2022-05-31 20:59:45 +0400

    movie app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,useEffect } from 'react'
+import Movie from './components/Movie';
 
-function App() {
+
+const FEATURED_API = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=fb1f301ce530a9bb513825b9f44b9df1'
+
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=fb1f301ce530a9bb513825b9f44b9df1&query="
+
+const App = () => {
+
+  const [movies,setMovies] = useState([])
+  const [searchTerm,setSearchTerm] = useState('')
+
+  useEffect(() => {
+    fetch(FEATURED_API).then(res => res.json())
+    .then(data => {
+      setMovies(data.results)
+    })
+  },[])
+
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    
+    fetch(SEARCH_API + searchTerm).then(res => res.json())
+    .then(data => {
+      setMovies(data.results)
+    })
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleOnSubmit}>
+        <header>
+          <input className="search" type="text" placeholder="Search..." value={searchTerm} onChange={e => setSearchTerm(e.target.value)} />
+        </header>
+      </form>
+      <div className="movie-container">
+        {movies.length > 0 &&
+          movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+      </div>
+    </>
   );
 }
 
-export default App;
+
+export default App
