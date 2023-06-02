a1106bb706e3253deaf703aa34e16998b50e6c8d
mchedlo14 <golocogo4@gmail.com>
2023-06-02 18:33:35 +0400

    add test file

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,6 +3,10 @@ const TRENDING_API = 'https://api.themoviedb.org/3/trending/movie/week?api_key=f
 const UPCOMING_API = 'https://api.themoviedb.org/3/movie/upcoming?api_key=fb1f301ce530a9bb513825b9f44b9df1&page=1';
 
 
+
+
+
+
 export const fetchPopularMovies = async () => {
   try {
     const response = await fetch(POPULAR_API);
